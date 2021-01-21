5bf258d62f8f59c584deda98f8ae1bbce5ebc19b
syzana <syzana.kajtazaj@gmail.com>
2021-01-21 21:55:51 +0100

    added first react challenges

---
+++ frontend-libraries/react/create_complex_jsx_element.js
@@ -0,0 +1,10 @@
+//JSX must return a single element 
+const JSX = <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
---
+++ frontend-libraries/react/create_simple_jsx_element.js
@@ -0,0 +1,7 @@
+// JSX allows user to write HTML directly within JS.
+// JSQ is not valid JS - it must be compiled into JS 
+// Babel is a tool for this process
+
+const JSX = <h1>Hello JSX!</h1>;
+
+ 
