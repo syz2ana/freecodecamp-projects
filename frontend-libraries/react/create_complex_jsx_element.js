c697336f4221558f825a632b7f5131cf800351b4
syzana <syzana.kajtazaj@gmail.com>
2021-01-21 22:11:22 +0100

    modified file

--- frontend-libraries/react/create_complex_jsx_element.js
+++ frontend-libraries/react/create_complex_jsx_element.js
@@ -1,10 +1,10 @@
 //JSX must return a single element 
 const JSX = <div>
-    <h1></h1>
-    <p></p>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-</div>
+                <h1></h1>
+                <p></p>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
