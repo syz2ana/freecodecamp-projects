e13ec8ec013eb070b34a4362b69650b1ccfdb58a
syzana <syzana.kajtazaj@gmail.com>
2021-01-24 18:21:27 +0100

    added next react challenges from frecodecamp

---
+++ frontend-libraries/react/create_stateful_component.js
@@ -0,0 +1,15 @@
+class StatefulComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: "Syzana"
+      }
+   }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
