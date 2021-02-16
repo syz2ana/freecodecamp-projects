088cf83ef8dbb5f51d390e831b8ff37397f32385
syzana <syzana.kajtazaj@gmail.com>
2021-02-16 10:36:05 +0100

    clean code

--- frontend-libraries/projects/random-quote-machine/random-quote-machine/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'; 
-
-const Button = ({buttonDisplayName, clickHandler}) => {
-    return <button onClick={clickHandler}>{buttonDisplayName}</button>
-}
-
-export default Button;
--- frontend-libraries/projects/random-quote-machine/random-quote-machine/src/components/QouteMachine.js
+++ frontend-libraries/projects/random-quote-machine/random-quote-machine/src/components/QouteMachine.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import Button from './Button';
+import { Card, CardActions, CardContent, Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const QouteMachine = (props) => {
-   return (
-        <React.Fragment>
-            {props.selectedQoute ? `${props.selectedQoute.author} - "${props.selectedQoute.qoute}"` : ''} 
-            <Button buttonDisplayName="Next Qoute" clickHandler={props.assignNewQouteIndex} />
-         </React.Fragment>
+const QouteMachine = ({ assignNewQouteIndex, selectedQoute }) => {
+    return (
+        <Card>
+            <CardContent>
+                <Typography id="text">
+                    {selectedQoute.quote} - <span id="author">{selectedQoute.author}</span> 
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button id="new-quote" size='small' onClick={assignNewQouteIndex}>Next Qoute</Button>
+                 <IconButton
+                    id="tweet-quote"
+                    target='_blank'
+                    href={encodeURI(`https://twitter.com/intent/tweet/tweet?text=${selectedQoute.quote}`)}> 
+                    <FontAwesomeIcon size='md' icon={faTwitter}>
+                    </FontAwesomeIcon> 
+                </IconButton>
+
+            </CardActions>
+        </Card>
     );
 }
 
